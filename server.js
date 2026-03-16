1f2aa105c2d978b96ba6c69270037cdf210a1b79
tznn013 <mohamed.touzani@edu.esiee.fr>
2026-03-16 17:16:05 +0100

    Confidentialité: Owner v8

--- server.js
+++ server.js
@@ -29,7 +29,9 @@ app.use(helmet({
     directives: {
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", "'unsafe-inline'"],
-      styleSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrcAttr: ["'unsafe-inline'"],
+      styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+      fontSrc: ["'self'", "https://fonts.gstatic.com"],
       imgSrc: ["'self'", "data:"],
     }
   }
